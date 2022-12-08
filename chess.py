2e6578f8123fe0e48baf876dab5edc1f20b7714d
Adam DiCioccio <adamdicioccio@gmail.com>
2022-12-08 03:43:36 -0500

    added square colors

--- chess.py
+++ chess.py
@@ -6,14 +6,18 @@ button_array={}
 
 def setup_tiles(root):
     index = 1
+    alternate = True
     for i in range(8):
         for j in range(8):
             def func(num=index):
                 select_tile(num)
+            color = ['#769656', '#eeeed2']
             inverse = ['1', '8', '7', '6', '5', '4', '3', '2', '1']
             letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
             display = f"{letters[math.floor((index/8)-0.01)]}{inverse[index%8]}"
-            button_array[i] = Button(root, text=display, command=func, height=4, width=9).grid(column=i, row=j)
+            button_array[i] = Button(root, text=display, command=func, height=4, width=9, bg=color[alternate]).grid(column=i, row=j)
+            if (index%8!=0):
+                alternate=not alternate
             index+=1
 
 
@@ -24,7 +28,6 @@ def select_tile(num):
     print(sq)
 
 
-
 def new():
     print("reset_board()")
 
@@ -33,7 +36,7 @@ root = Tk()
 setup_tiles(root)
 main_menu = Menu(root)
 root.config(menu=main_menu)
-main_menu.add_command(label='New', command=new)
+main_menu.add_command(label="New", command=new)
 root.geometry("586x570")
 root.title("Chess Game")
 root.config(bg="#111111")
