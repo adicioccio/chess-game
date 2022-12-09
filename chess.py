5ffbf50e019cb4940dc916f436e2509295c62f54
Adam DiCioccio <adamdicioccio@gmail.com>
2022-12-09 01:27:08 -0500

    added demo game pieces with semi-functionality

--- chess.py
+++ chess.py
@@ -1,26 +1,17 @@
 from tkinter import *
+from PIL import Image, ImageTk
 import math
 
-button_array={}
+
+root = Tk()
+
+w_bishop = PhotoImage(file=r"images/WB.png")
+b_bishop = PhotoImage(file=r"images/BB.png")
+
+tiles = {}
 
 
-# def setup_tiles(root):
-#     index = 1
-#     alternate = True
-#     for i in range(8):
-#         for j in range(8):
-#             def func(num=index):
-#                 select_tile(num)
-#             color = ['#769656', '#eeeed2']
-#             inverse = ['1', '8', '7', '6', '5', '4', '3', '2', '1']
-#             letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-#             display = f"{letters[math.floor((index/8)-0.01)]}{inverse[index%8]}"
-#             button_array[index-1] = Button(root, text=display, command=func, height=4, width=9, bg=color[alternate]).grid(column=i, row=j)
-#             if (index%8!=0):
-#                 alternate=not alternate
-#             index+=1
-
-def setup_tiles(root):
+def setup_tiles():
     index = 1
     alternate = True
     for i in range(8):
@@ -29,30 +20,29 @@ def setup_tiles(root):
             inverse = ['1', '8', '7', '6', '5', '4', '3', '2', '1']
             letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
             pos = f"{letters[math.floor((index/8)-0.01)]}{inverse[index%8]}"
-            btn = Button(root, text=pos, bg=color[alternate], activebackground="white", height=4, width=9)
+            btn = Button(root, text=pos, bg=color[alternate], activebackground="white", image=b_bishop)
             btn.grid(column=i, row=j)
-            button_array.setdefault(pos, btn)
-            button_array[pos].config(command=lambda key=button_array[pos]: select_tile(key))
+            tiles.setdefault(pos, btn)
+            tiles[pos].config(command=lambda key=tiles[pos]: select_tile(key))
             if (index%8!=0):
                 alternate=not alternate
             index+=1
 
 def select_tile(key):
-    key['text'] = 'new'
     text = key.cget('text')
-    print(key)
+    tiles[text].config(image=w_bishop)
+    print(text)
 
 
 def new():
     print("reset_board()")
 
 
-root = Tk()
-setup_tiles(root)
+setup_tiles()
 main_menu = Menu(root)
 root.config(menu=main_menu)
 main_menu.add_command(label="New", command=new)
-root.geometry("586x570")
+root.geometry("528x528")
 root.title("Chess Game - 0.0.0")
 root.config(bg="#111111")
 root.resizable(height=False, width=False)
